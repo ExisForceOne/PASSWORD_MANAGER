07bcc3cdc09f12d48dee446bfa10f92fcdac29b1
Damian Świadek <damian.swiadek@gmail.com>
2022-02-25 11:06:18 +0100

    FE: CopyBtn confirm msg

--- Client/src/Components/UI/CopyBtn/CopyBtn.js
+++ Client/src/Components/UI/CopyBtn/CopyBtn.js
@@ -1,10 +1,41 @@
 import style from './CopyBtn.module.css'
 import copyToClipboard from '../../../Helpers/copyToClipboard'
 import { FaCopy as CopyIcon } from 'react-icons/fa'
+import { useRef } from 'react'
 
 export default function CopyBtn(props){
 
+    const confirmMsg = useRef()
+
+    const showAndHideConfirmMsg = () =>{
+        const visibleTime = 1200 //ms
+        const span = confirmMsg.current;
+        
+        span.style.display= 'block';
+
+        setTimeout(()=>{
+            span.style.display= 'none';
+        },visibleTime)
+    }
+
+    function onClickHandler(){
+        showAndHideConfirmMsg()
+        copyToClipboard(props.value)
+    }
+
     return (
-        <button onClick={()=>copyToClipboard(props.value)} className={style.btn}><CopyIcon /></button>
+        <button
+         onClick={()=>onClickHandler()}
+         className={style.btn}
+        >
+            <CopyIcon />
+        
+            <span
+             className={style.confirmMsg}
+             ref={confirmMsg}
+             >Copied!
+            </span>
+            
+        </button>
     )
 }
