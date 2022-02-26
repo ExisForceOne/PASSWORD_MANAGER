b8ac69a7fe1b761624d47248112b40a17a10e6ed
Damian Świadek <damian.swiadek@gmail.com>
2022-02-26 10:18:18 +0100

    FE: back Btn

---
+++ Client/src/Components/UI/BackBtn/BackBtn.js
@@ -0,0 +1,23 @@
+export default function BackBtn(props){
+    
+
+    const styledBtn = {
+        padding: '10px 20px',
+        margin: '5px',
+        fontSize: 'inherit',
+        color: 'white',
+        borderRadius: '8px',
+        backgroundColor: '#dc3545',
+    }
+
+
+    return (
+        <button
+        style={styledBtn}
+        onClick={()=>props.onClick()}
+        >
+            {props.msg}
+        </button>
+    )
+      
+}
--- Client/src/Containers/Vault/Details/Details.js
+++ Client/src/Containers/Vault/Details/Details.js
@@ -1,14 +1,19 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 import Header from "../../../Components/Details/Header/Header"
 import { Login, Password, Url, Desc} from "../../../Components/Details/Section/Section"
+import BackBtn from "../../../Components/UI/BackBtn/BackBtn"
 import CopyBtn from '../../../Components/UI/CopyBtn/CopyBtn'
 import VisibleBtn from "../../../Components/UI/visibleBtn/VisibleBtn"
 import fakeData from '../../../fakeData'
 
 
 export default function Details(props){
+    let navigate = useNavigate()
     const [passwordIsVisible, setPasswordIsVisible] = useState(false)
+
+
 
 
     const data = fakeData[1]
@@ -18,6 +23,11 @@ export default function Details(props){
         color={data.color}
         name={data.name}
         fav={data.fav} />
+
+        <BackBtn
+         onClick={()=>{navigate(-1)}}
+         msg={'back'} 
+         />
 
         <Login value={data.login}>
             <CopyBtn value={data.login} />
