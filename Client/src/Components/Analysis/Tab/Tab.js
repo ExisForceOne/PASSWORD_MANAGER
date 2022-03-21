f243b374879fb30c43f80d35c23f211c168fe898
Damian Świadek <damian.swiadek@gmail.com>
2022-03-21 22:06:11 +0100

    Tab

---
+++ Client/src/Components/Analysis/Tab/Tab.js
@@ -0,0 +1,41 @@
+import KeysContainer from "../../KeysContainer/KeysContainer"
+import KeysItem from "../../KeysItem/KeysItem"
+import CollapseWeaknessList from '../CollapseWeaknessList/CollapseWeaknessList'
+
+export default function Tab(props) {
+    console.log(props.data[0].strength.weekness)
+
+    if(props.data[0].strength.weekness.length){
+        console.log('chuj')
+    }
+
+    return (
+        <KeysContainer 
+            header={
+                !props.data.length
+                ? 'You dont have any passwords in this category.'
+                : null
+            }
+        >
+            {
+            props.data.map(x=>
+                <div key={x._id}>
+                    <KeysItem
+                    key={x._id}
+                    name={x.name}
+                    color={x.color}
+                    fav={x.fav} 
+                    />
+                    {
+                        x.strength.weekness.length
+                        ? <CollapseWeaknessList weakness={x.strength.weekness} />
+                        : null
+                    }
+                    
+                </div>
+
+                )
+            }
+    </KeysContainer>
+    )
+}
