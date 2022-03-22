7b22fdc181355a8ae23304e5caf46f85b3c39ec5
Damian Świadek <damian.swiadek@gmail.com>
2022-03-22 21:59:05 +0100

    Analysis

--- Client/src/Containers/Analysis/Analysis.js
+++ Client/src/Containers/Analysis/Analysis.js
@@ -1,10 +1,57 @@
-const container = {
-    width: '100%',
-}
+import {useEffect, useState } from 'react'
+
+import fakeData from '../../fakeData'
+import Loading from '../../Components/Loading/Loading'
+import Chart from '../../Components/Analysis/Chart/Chart'
+import Menu from '../../Components/Analysis/Menu/Menu'
+import Tab from '../../Components/Analysis/Tab/Tab'
+
+
+import getArraysLenghtFromObj from '../../Helpers/getArraysLenghtFromObj'
+import classifieKeysStrength from '../../Features/classifieKeysStrength'
+
 
 export default function Analysis(props){
 
+    const [analyzedKeys, setAnalyzedKeys] = useState(null)
+    const [visibleTab, setVisibleTab] = useState('Weak')
+
+
+
+    useEffect(()=>{
+
+
+        const data = classifieKeysStrength(fakeData)
+        setAnalyzedKeys(data)
+
+
+
+
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+
+
+
+    if(!analyzedKeys) {
+        return <div><Loading /></div>
+    }
+
+
     return (
-        <p style={container}>Analysis</p>
+        <div>
+            <Chart 
+                data={getArraysLenghtFromObj(analyzedKeys)}
+                labels={Object.keys(analyzedKeys)}
+            />
+
+
+            <Menu data={Object.keys(analyzedKeys)} setVisibleTab={setVisibleTab} visibleTab={visibleTab} />
+
+
+            <Tab 
+                data={analyzedKeys[visibleTab]}
+            />
+        </div>
     )
 }
