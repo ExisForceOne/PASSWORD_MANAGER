4523d57e0221e02542eb20b7985063dbe677f3d0
Damian Świadek <damian.swiadek@gmail.com>
2022-04-03 12:14:35 +0200

    auth middleware, send token and verify

--- server/routes/keys.js
+++ server/routes/keys.js
@@ -1,9 +1,9 @@
 import express from "express";
 import keyController from "../controllers/keyController.js";
-
+import auth from "../middlewares/auth.js";
 const keys = express.Router();
 
-keys.post('/keys/add', keyController.add)
+keys.post('/keys/add', auth, keyController.add)
 // keys.post('/users/login', userController.login)
 
 export default keys
